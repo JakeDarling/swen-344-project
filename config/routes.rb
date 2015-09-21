3445db3d48ad43d7a45b23f9cc35ff0afb7dd051
Jake Darling <jakedarling1@gmail.com>
2015-09-21 16:57:26 -0400

    Add routes for index and stock

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/' => 'epic#index'
+  get '/stocks' => 'stocks#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

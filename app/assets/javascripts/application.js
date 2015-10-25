17e995849e9e0bdc7ca7ef1e16aa99411f103a22
Derek Leung <xdmds200092@gmail.com>
2015-10-24 23:56:04 -0400

    moved window refresh so it doesn't interrupt stock buy process

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -220,179 +220,180 @@ function printQuote(data) {
     $('#quoteResults').append(stockxchangeDiv);
 }
 
-    function validateBuyForm(){
-        var ticker = $('#buy-ticker').val().toUpperCase();
-        var shares = $('#buy-shares').val();
-        var sReg = new RegExp('^[0-9]*$');
-        var tReg = new RegExp('^[a-zA-Z]+$');
+function validateBuyForm(){
+    var ticker = $('#buy-ticker').val().toUpperCase();
+    var shares = $('#buy-shares').val();
+    var sReg = new RegExp('^[0-9]*$');
+    var tReg = new RegExp('^[a-zA-Z]+$');
 
-        //validate inputs
-        if (ticker=='' || shares=='' || !sReg.test(shares) || !tReg.test(ticker)) {
-            alert("invalid field(s)");
-        } else {
-            getStockPrice(ticker, shares);
-            //alert("purchased");
-        }
+    //validate inputs
+    if (ticker=='' || shares=='' || !sReg.test(shares) || !tReg.test(ticker)) {
+        alert("invalid field(s)");
+    } else {
+        getStockPrice(ticker, shares);
+        //alert("purchased");
     }
+}
 
-    function postBuyForm(ticker, shares, price){
-        var pReg = new RegExp("^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$");
+function postBuyForm(ticker, shares, price){
+    var pReg = new RegExp("^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$");
 
-        if(pReg.test(price)) {
-            $.ajax({
-                type:'POST',
-                url:'/buy-stock',
-                data:{
-                    'ticker_symbol': ticker,
-                    'shares': shares,
-                    'price': price,
-                },
-                success: function(){
-                    alert("Successfully purchased");
-                }
-            });
-        } else {
-            alert("No such stock ticker");
-        }
-
-        
-    }
-
-    function getStockPrice(ticker, shares){
-        var price;
+    if(pReg.test(price)) {
         $.ajax({
+            type:'POST',
+            url:'/buy-stock',
+            data:{
+                'ticker_symbol': ticker,
+                'shares': shares,
+                'price': price,
+            },
+            success: function(){
+                alert("Successfully purchased");
+                window.location.reload();
+            }
+        });
+    } else {
+        alert("No such stock ticker");
+    }
+
+    
+}
+
+function getStockPrice(ticker, shares){
+    var price;
+    $.ajax({
+        dataType: "xml",
+        type: 'GET',
+        url: 'https://query.yahooapis.com/v1/public/yql/derekleung/getQuote',
+        data: {
+            diagnostics: 'true',
+            env: 'store://datatables.org/alltableswithkeys',
+            symbol: ticker,
+        },
+        success: function (data) {
+            //printTicker(data, 'ndaq');
+            price = parseFloat($(data).find('LastTradePriceOnly').text()).toFixed(2);
+            postBuyForm(ticker, shares, price);
+        }
+    });
+}
+
+function buildStockTable(data){
+    //$('#debug-output').html(JSON.stringify(data));
+    var stocks = data['stocks'];
+    var sArr = [];
+    var row;
+    var numStocks = Object.keys(stocks).length
+    var index = 0;
+
+    $.each(stocks, function(index, value){
+        index ++;
+        $.ajax({
+            async: false,
             dataType: "xml",
             type: 'GET',
             url: 'https://query.yahooapis.com/v1/public/yql/derekleung/getQuote',
             data: {
                 diagnostics: 'true',
                 env: 'store://datatables.org/alltableswithkeys',
-                symbol: ticker,
+                symbol: value['ticker_symbol'],
             },
             success: function (data) {
-                //printTicker(data, 'ndaq');
-                price = parseFloat($(data).find('LastTradePriceOnly').text()).toFixed(2);
-                postBuyForm(ticker, shares, price);
-            }
+                buildRows(data, value, sArr, index, numStocks, totalChange);
+            },
         });
-    }
-
-    function buildStockTable(data){
-        //$('#debug-output').html(JSON.stringify(data));
-        var stocks = data['stocks'];
-        var sArr = [];
-        var row;
-        var numStocks = Object.keys(stocks).length
-        var index = 0;
-
-        $.each(stocks, function(index, value){
-            index ++;
-            $.ajax({
-                async: false,
-                dataType: "xml",
-                type: 'GET',
-                url: 'https://query.yahooapis.com/v1/public/yql/derekleung/getQuote',
-                data: {
-                    diagnostics: 'true',
-                    env: 'store://datatables.org/alltableswithkeys',
-                    symbol: value['ticker_symbol'],
-                },
-                success: function (data) {
-                    buildRows(data, value, sArr, index, numStocks, totalChange);
-                },
-            });
-            
-        });
-    }
-
-    function buildRows(data, value, sArr, index, numStocks, totalChange){
-        var avgDailyVol = $(data).find('AverageDailyVolume').text();
-        var change = $(data).find('Change').text();
-        var sign = change.substring(0, 1);
-        change = change.substring(1);
-        if(sign=="-"){
-            change *= -1;
-        }
-        var daysLow = $(data).find('DaysLow').text();
-        var daysHigh = $(data).find('DaysHigh').text();
-        var yearLow = $(data).find('YearLow').text();
-        var yearHigh = $(data).find('YearHigh').text();
-        var marketCap = parseFloat($(data).find('MarketCapitalization').text()).toFixed(2);
-        var lastTradePrice = parseFloat($(data).find('LastTradePriceOnly').text()).toFixed(2);
-        var daysRange = $(data).find('DaysRange').text();
-        var name = $(data).find('Name').text();
-        var symbol = $(data).find('Symbol').text();
-        var volume = $(data).find('Volume').text();
-        var stockxchange = $(data).find('StockExchange').text();
         
-        var gain = ((lastTradePrice * value['shares']) - value['base_cost']).toFixed(2);
-        var gain_pc = ((gain/value['base_cost']) * 100).toFixed(2);
-        var days_gain = 'TBD'//change * value['shares'];
+    });
+}
 
-        //update totals
-
-        window.totalChange = (parseFloat(window.totalChange) + parseFloat(change)).toFixed(2);
-        window.totalMarketCap = (parseFloat(window.totalMarketCap) + parseFloat(marketCap)).toFixed(2);
-        window.totalGain = (parseFloat(window.totalGain) + parseFloat(gain)).toFixed(2);
-        window.totalGainPc = (parseFloat(window.totalGainPc) + parseFloat(gain_pc)).toFixed(2);
-        window.totalDaysGain = "TBD";
-        row = [
-            name,
-            symbol,
-            lastTradePrice,
-            sign + '' + change,
-            value['shares'],
-            marketCap,
-            gain,
-            gain_pc,
-            days_gain
-        ];
-        sArr.push(row);
-        if(index==numStocks){
-            var options = {}
-            options.data = sArr;
-            options.columns = [
-                    { title: "Name" },
-                    { title: "Symbol" },
-                    { title: "Last Trade Price" },
-                    { title: "Change" },
-                    { title: "Shares" },
-                    { title: "Market Cap." },
-                    { title: "Gain" },
-                    { title: "Gain %" },
-                    { title: "Day's Gain" },
-                ];
-            options.fnInitComplete = function(){
-                $('#myTable tfoot').prepend(                
-                    '<tr>' +
-                        '<th>' +
-                            'Portfolio Value' +
-                        '</th>' +
-                        '<th></th><th></th>' + 
-                        '<th>' +
-                            window.totalChange +
-                        '</th>' +
-                        '<th>' +
-                        '</th>' +
-                        '<th>' +
-                            window.totalMarketCap +
-                        '</th>' +
-                        '<th>' +
-                            window.totalGain +
-                        '</th>' +
-                        '<th>' +
-                            window.totalGainPc +
-                        '</th>' +
-                        '<th>' +
-                            window.totalDaysGain +
-                        '</th>' +
-                    '</tr>'
-                );
-            };
-
-            $('#myTable').DataTable(options);
-        }
+function buildRows(data, value, sArr, index, numStocks, totalChange){
+    var avgDailyVol = $(data).find('AverageDailyVolume').text();
+    var change = $(data).find('Change').text();
+    var sign = change.substring(0, 1);
+    change = change.substring(1);
+    if(sign=="-"){
+        change *= -1;
     }
+    var daysLow = $(data).find('DaysLow').text();
+    var daysHigh = $(data).find('DaysHigh').text();
+    var yearLow = $(data).find('YearLow').text();
+    var yearHigh = $(data).find('YearHigh').text();
+    var marketCap = parseFloat($(data).find('MarketCapitalization').text()).toFixed(2);
+    var lastTradePrice = parseFloat($(data).find('LastTradePriceOnly').text()).toFixed(2);
+    var daysRange = $(data).find('DaysRange').text();
+    var name = $(data).find('Name').text();
+    var symbol = $(data).find('Symbol').text();
+    var volume = $(data).find('Volume').text();
+    var stockxchange = $(data).find('StockExchange').text();
+    
+    var gain = ((lastTradePrice * value['shares']) - value['base_cost']).toFixed(2);
+    var gain_pc = ((gain/value['base_cost']) * 100).toFixed(2);
+    var days_gain = 'TBD'//change * value['shares'];
+
+    //update totals
+
+    window.totalChange = (parseFloat(window.totalChange) + parseFloat(change)).toFixed(2);
+    window.totalMarketCap = (parseFloat(window.totalMarketCap) + parseFloat(marketCap)).toFixed(2);
+    window.totalGain = (parseFloat(window.totalGain) + parseFloat(gain)).toFixed(2);
+    window.totalGainPc = (parseFloat(window.totalGainPc) + parseFloat(gain_pc)).toFixed(2);
+    window.totalDaysGain = "TBD";
+    row = [
+        name,
+        symbol,
+        lastTradePrice,
+        sign + '' + change,
+        value['shares'],
+        marketCap,
+        gain,
+        gain_pc,
+        days_gain
+    ];
+    sArr.push(row);
+    if(index==numStocks){
+        var options = {}
+        options.data = sArr;
+        options.columns = [
+                { title: "Name" },
+                { title: "Symbol" },
+                { title: "Last Trade Price" },
+                { title: "Change" },
+                { title: "Shares" },
+                { title: "Market Cap." },
+                { title: "Gain" },
+                { title: "Gain %" },
+                { title: "Day's Gain" },
+            ];
+        options.fnInitComplete = function(){
+            $('#myTable tfoot').prepend(                
+                '<tr>' +
+                    '<th>' +
+                        'Portfolio Value' +
+                    '</th>' +
+                    '<th></th><th></th>' + 
+                    '<th>' +
+                        window.totalChange +
+                    '</th>' +
+                    '<th>' +
+                    '</th>' +
+                    '<th>' +
+                        window.totalMarketCap +
+                    '</th>' +
+                    '<th>' +
+                        window.totalGain +
+                    '</th>' +
+                    '<th>' +
+                        window.totalGainPc +
+                    '</th>' +
+                    '<th>' +
+                        window.totalDaysGain +
+                    '</th>' +
+                '</tr>'
+            );
+        };
+
+        $('#myTable').DataTable(options);
+    }
+}
 /*****************************************************************************/
 /* END STOCKS*/
 /*****************************************************************************/

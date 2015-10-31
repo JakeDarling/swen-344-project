ed8d128dc1fe1764160f44e4bd467e361d820bea
Derek Leung <xdmds200092@gmail.com>
2015-10-31 14:04:53 -0400

    fixed migrations

--- db/migrate/20151025010349_create_events.rb
+++ db/migrate/20151025010349_create_events.rb
@@ -2,15 +2,11 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.datetime :start
-      t.datetime :end
+      t.datetime :end1
       t.string :description
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
-  end
-
-  def up
-  	rename_column :events, :end, :end1
   end
 end
--- db/migrate/20151026204524_rename_column_endin_table_events2.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnEndinTableEvents2 < ActiveRecord::Migration
-  def up
-  	rename_column :events, :end, :end1
-  end
-end

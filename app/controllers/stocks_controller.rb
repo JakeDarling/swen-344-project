ce305f80be1b71c380bb3e1a7ba1869fa94ee7a3
Derek Leung <xdmds200092@gmail.com>
2015-11-02 16:39:54 -0500

    server side validation for buying and selling stocks is done

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -37,64 +37,59 @@ class StocksController < ApplicationController
     user = User.find_by(fbUserId:session[:user])
 
     #input validation
-    sReg = /^[0-9]*$/
-    tReg = /^[a-zA-Z]+$/
-    pReg = /^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$/
+    if validate_stock_input(shares, ticker, price)
 
-    if ticker == '' or !tReg.match(ticker) or shares == '' or !sReg.match(shares) or pReg == '' or !pReg.match(price)
+      #record the transaction
+      tran = Transaction.new( ticker_symbol:ticker, \
+                              user_id:user.id, \
+                              timestamp:DateTime.now, \
+                              price:price.to_f.round(2), \
+                              shares:shares \
+                            )
+      if tran.valid?
+        tran.save()
+      else
+        #TODO: render failure template but this shouldn't happen since client validates
+        puts "=========================================="
+        puts "Transaction failed. transaction entry invalid."
+        puts "=========================================="
+      end
+      puts tran.inspect
+
+      #update Stock table
+      stock = Stock.find_by(user_id:user.id, ticker_symbol:ticker)
+      cost = (shares.to_i * price.to_f).round(2)
+      if stock != nil
+        stock.shares += shares.to_i
+        stock.base_cost += cost
+      else
+        stock = Stock.new(ticker_symbol:ticker, shares:shares, user_id:user.id, base_cost:cost)
+      end
+      
+      if stock.valid?
+        stock.save()
+
+        #log to console
+        puts "==================================="
+        puts "Transaction Complete"
+        puts "user id: #{user.id}"
+        puts "user fbUserId: #{user.fbUserId}"
+        puts "stock: #{stock.ticker_symbol}"
+        puts "shares: #{tran.shares}"
+        puts "price: #{tran.price}"
+
+        puts "shares held: #{stock.shares}"
+        puts "base cost: #{stock.base_cost}"
+        puts "==================================="
+      else
+        puts "=========================================="
+        puts "Transaction failed. stock entry invalid."
+        puts "=========================================="
+      end
+    else
       puts "============================="
       puts "invalid inputs. return false"
       puts "============================="
-      render:nothing => true
-      return false
-    end
-
-    #record the transaction
-    tran = Transaction.new( ticker_symbol:ticker, \
-                            user_id:user.id, \
-                            timestamp:DateTime.now, \
-                            price:price.to_f.round(2), \
-                            shares:shares \
-                          )
-    if tran.valid?
-      tran.save()
-    else
-      #TODO: render failure template but this shouldn't happen since client validates
-      puts "=========================================="
-      puts "Transaction failed. transaction entry invalid."
-      puts "=========================================="
-    end
-    puts tran.inspect
-
-    #update Stock table
-    stock = Stock.find_by(user_id:user.id, ticker_symbol:ticker)
-    cost = (shares.to_i * price.to_f).round(2)
-    if stock != nil
-      stock.shares += shares.to_i
-      stock.base_cost += cost
-    else
-      stock = Stock.new(ticker_symbol:ticker, shares:shares, user_id:user.id, base_cost:cost)
-    end
-    
-    if stock.valid?
-      stock.save()
-
-      #log to console
-      puts "==================================="
-      puts "Transaction Complete"
-      puts "user id: #{user.id}"
-      puts "user fbUserId: #{user.fbUserId}"
-      puts "stock: #{stock.ticker_symbol}"
-      puts "shares: #{tran.shares}"
-      puts "price: #{tran.price}"
-
-      puts "shares held: #{stock.shares}"
-      puts "base cost: #{stock.base_cost}"
-      puts "==================================="
-    else
-      puts "=========================================="
-      puts "Transaction failed. stock entry invalid."
-      puts "=========================================="
     end
 
     render:nothing => true
@@ -112,68 +107,91 @@ class StocksController < ApplicationController
 =end
   def sell_stock
     ticker = params[:ticker_symbol]
-    shares = params[:shares].to_i * -1
-    price = params[:price].to_f.round(2)
+    shares = params[:shares]
+    price = params[:price]
     user = User.find_by(fbUserId:session[:user])
 
-    #record the transaction
-    tran = Transaction.new( ticker_symbol:ticker, \
-                            user_id:user.id, \
-                            timestamp:DateTime.now, \
-                            price:price, \
-                            shares:shares \
-                          )
-    if tran.valid?
-      tran.save()
+    #input validation
+    if validate_stock_input(shares, ticker, price)
+
+      shares = shares.to_i * -1
+      price = price.to_f.round(2)
+
+      #record the transaction
+      tran = Transaction.new( ticker_symbol:ticker, \
+                              user_id:user.id, \
+                              timestamp:DateTime.now, \
+                              price:price, \
+                              shares:shares \
+                            )
+      if tran.valid?
+        tran.save()
+      else
+        #TODO: render failure template but this shouldn't happen since client validates
+        puts "=========================================="
+        puts "Transaction failed. transaction entry invalid."
+        puts "=========================================="
+      end
+      puts tran.inspect
+
+      #update Stock table
+      stock = Stock.find_by(user_id:user.id, ticker_symbol:ticker)
+      cost = (shares * price).round(2)
+      if stock != nil
+        stock.shares += shares
+        stock.base_cost += cost
+      else
+        #user doesn't own this stock
+        ##TODO: render failure template but this shouldn't happen since client validates
+        puts "=========================================="
+        puts "Transaction failed. transaction entry invalid."
+        puts "=========================================="
+      end
+      
+      if stock.valid?
+        stock.save()
+              #log to console
+        puts "==================================="
+        puts "Transaction Complete"
+        puts "user id: #{user.id}"
+        puts "user fbUserId: #{user.fbUserId}"
+        puts "stock: #{stock.ticker_symbol}"
+        puts "shares: #{tran.shares}"
+        puts "price: #{tran.price}"
+
+        puts "shares held: #{stock.shares}"
+        puts "base cost: #{stock.base_cost}"
+        puts "==================================="
+      else
+        puts "=========================================="
+        puts "Transaction failed. stock entry invalid."
+        puts "=========================================="
+      end
+
+      if stock.shares <= 0
+        stock.destroy()
+        puts "==========================================================="
+        puts "User no longer owns any shares of this stock. destroying..."
+        puts "==========================================================="
+      end
+
     else
-      #TODO: render failure template but this shouldn't happen since client validates
-      puts "=========================================="
-      puts "Transaction failed. transaction entry invalid."
-      puts "=========================================="
+      puts "============================="
+      puts "invalid inputs. return false"
+      puts "============================="
     end
-    puts tran.inspect
-
-    #update Stock table
-    stock = Stock.find_by(user_id:user.id, ticker_symbol:ticker)
-    cost = (shares * price).round(2)
-    if stock != nil
-      stock.shares += shares
-      stock.base_cost += cost
-    else
-      #user doesn't own this stock
-      ##TODO: render failure template but this shouldn't happen since client validates
-      puts "=========================================="
-      puts "Transaction failed. transaction entry invalid."
-      puts "=========================================="
-    end
-    
-    if stock.valid?
-      stock.save()
-            #log to console
-      puts "==================================="
-      puts "Transaction Complete"
-      puts "user id: #{user.id}"
-      puts "user fbUserId: #{user.fbUserId}"
-      puts "stock: #{stock.ticker_symbol}"
-      puts "shares: #{tran.shares}"
-      puts "price: #{tran.price}"
-
-      puts "shares held: #{stock.shares}"
-      puts "base cost: #{stock.base_cost}"
-      puts "==================================="
-    else
-      puts "=========================================="
-      puts "Transaction failed. stock entry invalid."
-      puts "=========================================="
-    end
-
-    if stock.shares <= 0
-      stock.destroy()
-      puts "==========================================================="
-      puts "User no longer owns any shares of this stock. destroying..."
-      puts "==========================================================="
-    end
-
     render:nothing => true
+  end
+
+  def validate_stock_input(shares, ticker, price)
+    sReg = /^[0-9]*$/
+    tReg = /^[a-zA-Z]+$/
+    pReg = /^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$/
+
+    if ticker == '' or !tReg.match(ticker) or shares == '' or !sReg.match(shares) or pReg == '' or !pReg.match(price)
+      return false
+    else
+      return true
+    end
   end
 end

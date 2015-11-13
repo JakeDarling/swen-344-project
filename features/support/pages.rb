4a65ef65cd440ca4778826448312a9fcb92b8a0f
Jake Darling <jakedarling1@gmail.com>
2015-11-12 19:20:12 -0500

    Finalize buy and search stock tests

--- features/support/pages.rb
+++ features/support/pages.rb
@@ -1,6 +1,30 @@
 
 # Implementation of the page object model for this project
 # In a real workplace this would be more organized and use modules
+HOME ||= '/'
+CALENDAR ||= '/calendar'
+STOCKS ||= '/stocks'
+MY_STOCKS ||= '/my-stocks'
+TRANSACTION_HISTORY ||= '/transactions'
+
+# Navigation bar widget
+class Navigation
+  def initialize(browser)
+    @b = browser
+  end
+  def home
+    @b.link(:href => HOME).click
+  end
+  def calendar
+    @b.link(:href => CALENDAR).click
+  end
+  def stocks
+    @b.link(:href => STOCKS).click
+  end
+  def my_stocks
+    @b.link(:href => MY_STOCKS).click
+  end
+end
 
 # Epic index page
 class Dashboard
@@ -60,22 +84,62 @@ class Stocks
     @b.text_field(:name => 'symbol').set symbol
   end
   def has_current?
-    pending 'Not sure if this is table cell or what'
+    @b.div(:id => 'lastTradePrice').exists?
   end
-  def has_day_high
-    pending 'Not sure if this is table cell or what'
+  def has_day_high?
+    @b.div(:id => 'daysHigh').exists?
   end
-  def has_day_low
-    pending 'Not sure if this is table cell or what'
+  def has_day_low?
+    @b.div(:id => 'daysLow').exists?
   end
-  def has_year_high
-    pending 'Not sure if this is table cell or what'
+  def has_year_high?
+    @b.div(:id => 'yearHigh').exists?
   end
-  def has_year_low
-    pending 'Not sure if this is table cell or what'
+  def has_year_low?
+    @b.div(:id => 'yearLow').exists?
   end
   def has_chart?
-    pending 'Not sure if this is table cell or what'
+    @b.div(:id => 'chart').exists?
+  end
+end
+
+class MyStocks
+  def initialize(browser)
+    @b = browser
+  end
+  def transaction_history
+    @b.link(:href => TRANSACTION_HISTORY).click
+  end
+  def buy
+    @b.link(:id => 'first-buy').click
+  end
+  def set_ticker(symbol)
+    @b.text_field(:id => 'buy-ticker').when_present.set symbol
+  end
+  def set_shares(num_shares)
+    @b.text_field(:id => 'buy-shares').when_present.set num_shares
+  end
+  # Confirm in the modal
+  def confirm_purchase
+    @b.link(:id => 'buy-submit').when_present.click
+  end
+end
+
+class Transactions
+  def initialize(browser)
+    @b = browser
+  end
+  # Return the ticker symbol of the nth row
+  def symbol(index)
+    @b.tr(:index => index).td(:index => 1).when_present.text
+  end
+  # Return number of shares for the nth row
+  def num_shares(index)
+    @b.tr(:index => index).td(:index => 3).when_present.text
+  end
+  # Return true if this was a purchase, false if sale
+  def bought?(index)
+    @b.tr(:index => index).td(:index => 4).text == 'buy'
   end
 end
 

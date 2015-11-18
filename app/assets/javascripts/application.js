8476dcc5d46ec7de0758a0d9df4569bd0d6bd98a
Brendan Kirby <bjk7706@gmail.com>
2015-11-18 17:48:37 -0500

    Installs jquery-ui gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.foundation

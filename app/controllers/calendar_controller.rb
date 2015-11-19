043a3bc4a9516898bed3066580bedbd89871b4f1
Brendan Kirby <bjk7706@gmail.com>
2015-11-18 20:59:38 -0500

    Adds front and back end validation

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,29 +3,53 @@ class CalendarController < ApplicationController
 	end
 
 	def get_my_events
-	    user = User.find_by(fbUserId:session[:user])
-	    events = Event.where(user_id:user.id)
+    user = User.find_by(fbUserId:session[:user])
+    events = Event.where(user_id:user.id)
 
-	    render :json => {events:events}
-	  end
+    render :json => {events:events}
+	end
 
 	def store_event
+		puts "** Storing Event **"
 		title = params[:title]
 		start = params[:start]
 		end1 = params[:end1]
 
 		user = User.find_by(fbUserId:session[:user])
 
-		event = Event.new(title: title, start: start, end1: end1, user_id: user.id)
+		if validate_event_input(title, start, end1)
+      event = Event.new(title: title, start: start, end1: end1, user_id: user.id)
 
-		if event.valid?
-			event.save()
-		else
-			# failure
-		end
-
-		puts event.inspect
+      if event.valid?
+      	puts "** saved event **"
+        event.save()
+      else
+        puts "================================================"
+        puts "Storing Event failed. Event data entry invalid."
+        puts "================================================"
+      end
+      puts event.inspect
+	  else
+      puts "============================="
+      puts "invalid inputs. return false"
+      puts "============================="
+	  end
 
 		render:nothing => true
 	end
+
+	def validate_event_input(title, start, end1)
+		puts "** validating event **"
+    tReg = /^.{0,100}$/
+    sReg = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+    eReg = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+
+    if title == '' or !tReg.match(title) or start == '' or !sReg.match(start) or end1 == '' or !eReg.match(end1)
+      puts "** not valid **"
+      return false
+    else
+    	puts "** valid **"
+      return true
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+	validates :title, presence:true
+	validates :start, presence:true
+	validates :end1, presence:true
 end

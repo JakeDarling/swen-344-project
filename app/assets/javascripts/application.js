e7ad39007412896f0a9ed2505bf4589aae79c529
Brendan Kirby <bjk7706@gmail.com>
2015-12-06 16:23:28 -0500

    Fixes upload btn style to match fullcalendar buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1494,7 +1494,7 @@ function renderCalendar() {
         }
     });
 
-    $('.fc-left').append($('<a data-reveal-id="uploadEventModal" class="button tiny radius">upload</a>'));
+    $('.fc-left').append($('<a data-reveal-id="uploadEventModal" id="uploadEventBtn" class="fc button tiny radius">upload</a>'));
     document.getElementById('eventTxtFileUpload').addEventListener('change', uploadEvent, false);
 }
 
